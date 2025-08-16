afd843fa74a2ffc87fd087955b1a122e5a1df3e8
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2025-08-16 06:11:16 +0000

    Refactor main function to use structured messages for content generation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
 
 
@@ -12,16 +13,26 @@ def main():
         print("Usage: python main.py <contents>")
         sys.exit(1)
 
-    contents = sys.argv[1]
+    is_verbose = "--verbose" in sys.argv
+
+    user_prompt = sys.argv[1]
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
 
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(  # pyright: ignore[reportUnknownMemberType]
         model="gemini-2.0-flash-001",
-        contents=contents,
+        contents=messages,
     )
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if is_verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(
+            f"Response tokens: {response.usage_metadata.candidates_token_count}"
+        )
 
 
 if __name__ == "__main__":
