95ded027572f80bacd9fc8335a2b33605cfdaa85
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2025-08-16 08:31:29 +0000

    Add schema definition for get_file_content function

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,5 @@
 import os
-
+from google.genai import types
 from config import MAX_FILE_LENGTH
 
 
@@ -21,3 +21,19 @@ def get_file_content(working_directory: str, file_path: str) -> str | None:
             return content
     except Exception as e:
         return f"Error reading file: {e}"
+
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description=f"Reads and returns the first {MAX_FILE_LENGTH} characters of the content from a specified file within the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file whose content should be read, relative to the working directory.",
+            ),
+        },
+        required=["file_path"],
+    ),
+)
